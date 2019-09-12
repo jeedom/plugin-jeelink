f2fa43ba3c823b0ca126769fa90eab81f382f9f5
Loïc <loic@jeedom.com>
2019-09-12 11:19:00 +0200

    Update jeelink.php

--- desktop/php/jeelink.php
+++ desktop/php/jeelink.php
@@ -34,9 +34,11 @@ foreach ($eqLogics as $eqLogic) {
 </div>
 
 <div class="col-xs-12 eqLogic" style="display: none;">
-	<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+<div class="input-group pull-right" style="display:inline-flex">
+	<span class="input-group-btn">
+		<a class="btn btn-default btn-sm eqLogicAction roundedLeft" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+	</span>
+</div>
   <ul class="nav nav-tabs" role="tablist">
     <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
@@ -107,8 +109,8 @@ foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
   </fieldset>
 </form>
 </div>
-<div role="tabpanel" class="tab-pane" id="commandtab">
-  <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
+<div role="tabpanel" class="tab-pane" id="commandtab"> 
+  <br/>
   <table id="table_cmd" class="table table-bordered table-condensed">
     <thead>
       <tr>
